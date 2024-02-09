c956c6dace58cf6252c6f75cda836cd1d6698856
Guillaume Antier <guillaume.antier@ynov.com>
2024-02-09 08:22:52 +0100

    Change the meteo function

--- commands/utility/meteo.js
+++ commands/utility/meteo.js
@@ -1,7 +1,4 @@
-const Discord = require("discord.js");
-const config = require("../../config")
-const intents = new Discord.IntentsBitField(3276799)
-const bot = new Discord.Client({intents})
+const { SlashCommandBuilder } = require('discord.js');
 
 
 const parameters = {
@@ -13,10 +10,10 @@ const parameters = {
 bot.login(config.token)
 
 module.exports =  {
-
-    name: "meteo",
-
-    async run (bot, message)  {
+    data: new SlashCommandBuilder()
+        .setName('meteo')
+        .setDescription('Provides information about the meteo.'),
+    async execute(interaction) {
         try {
             const response = await fetch(`http://api.weatherstack.com/current?access_key=${parameters.access_key}&query=${parameters.query}&units=${parameters.units}`);
             const data = await response.json();
@@ -25,7 +22,7 @@ module.exports =  {
             const messageContent = `La température à ${location.name} est de ${current.temperature}°C, le ressenti est de ${current.feelslike}°C. Le temps actuel est : ${current.weather_descriptions.join(', ')}.`;
 
             console.log(messageContent); 
-            message.channel.send(messageContent);
+            await interaction.reply(messageContent);
         } catch (error) {
             console.error('Erreur, veuillez réessayer :', error);
         }
